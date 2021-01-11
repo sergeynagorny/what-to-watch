9c2f8876e650303a6f80de48396d4a1d5e183784
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-01-11 17:21:18 +0300

    Обновил е2е-тесты для компонент
    - main-page.e2e.test.js
    - movie-card.e2e.test.js

--- src/components/main-page/main-page.e2e.test.js
+++ src/components/main-page/main-page.e2e.test.js
@@ -8,12 +8,36 @@ Enzyme.configure({
   adapter: new Adapter(),
 });
 
-it(`Should «Show More» button be pressed`, () => {
+const films = [
+  {
+    title: `What We Do in the Shadows`,
+    poster: `img/what-we-do-in-the-shadows.jpg`,
+  },
+  {
+    title: `Pulp Fiction`,
+    poster: `img/pulp-fiction.jpg`,
+  },
+  {
+    title: `No Country for Old Men`,
+    poster: `img/no-country-for-old-men.jpg`,
+  },
+  {
+    title: `Moonrise Kingdom`,
+    poster: `img/moonrise-kingdom.jpg`,
+  },
+  {
+    title: `Orlando`,
+    poster: `img/orlando.jpg`,
+  },
+];
+
+
+it(`Should "Show More" button be pressed`, () => {
   const onShowMoreButtonClick = jest.fn();
 
   const mainPage = shallow(
       <MainPage
-        movieTitles={[]}
+        films={films}
         onShowMoreButtonClick={onShowMoreButtonClick}
       />
   );
---
+++ src/components/movie-card/movie-card.e2e.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import MovieCard from "./movie-card";
+
+
+configure({
+  adapter: new Adapter()
+});
+
+const film = {
+  title: `What We Do in the Shadows`,
+  poster: `img/what-we-do-in-the-shadows.jpg`,
+};
+
+it(`When hovering card passed to callback is consistent with "activeFilm" MovieList state`, () => {
+  const onMovieCardHover = jest.fn();
+
+  const movieCard = shallow(
+      <MovieCard
+        film={film}
+        onMovieCardHover={onMovieCardHover}
+      />
+  );
+
+  movieCard.simulate(`mouseenter`);
+
+  expect(onMovieCardHover).toHaveBeenCalledTimes(1);
+  expect(onMovieCardHover).toHaveBeenCalledWith(film);
+});

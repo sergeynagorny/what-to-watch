4fa4dbeea0f44d8e4eeab4b8b934017026752a71
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-01-19 14:16:25 +0300

    Обновил `const.js`

--- src/const.js
+++ src/const.js
@@ -4,12 +4,28 @@ export const MovieTab = {
   REVIEWS: `Reviews`,
 };
 
-export const MovieDetailsItemName = {
+export const GenresList = {
+  ALL: `All genres`,
+  COMEDIES: `Comedies`,
+  CRIME: `Crime`,
+  DOCUMENTARY: `Documentary`,
+  DRAMAS: `Dramas`,
+  HORROR: `Horror`,
+  KIDS_AND_FAMILY: `Kids &amp; Family`,
+  ROMANCE: `Romance`,
+  SCI_FI: `Sci-Fi`,
+  THRILLERS: `Thrillers`,
+};
+
+export const MovieCardType = {
+  FULL: `full`,
+  PREVIEW: `preview`,
+};
+
+export const TabsDetailsItemName = {
   DIRECTOR: `Director`,
   STARRING: `Starring`,
   RUNTIME: `Run Time`,
   GENRE: `Genre`,
   RELEASED: `Released`,
 };
-
-export const movieTabs = Object.values(MovieTab);

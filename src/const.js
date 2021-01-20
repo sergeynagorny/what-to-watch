b7563c9804524981c56a72fc555ee4b0c0bda232
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-01-20 21:03:06 +0300

    Рефакторинг `const.js`

--- src/const.js
+++ src/const.js
@@ -4,19 +4,6 @@ export const MovieTab = {
   REVIEWS: `Reviews`,
 };
 
-export const GenresList = {
-  ALL: `All genres`,
-  COMEDIES: `Comedies`,
-  CRIME: `Crime`,
-  DOCUMENTARY: `Documentary`,
-  DRAMAS: `Dramas`,
-  HORROR: `Horror`,
-  KIDS_AND_FAMILY: `Kids &amp; Family`,
-  ROMANCE: `Romance`,
-  SCI_FI: `Sci-Fi`,
-  THRILLERS: `Thrillers`,
-};
-
 export const MovieCardType = {
   FULL: `full`,
   PREVIEW: `preview`,

5cc0428319cbb87c3c8132472b299be508d7c92b
sergeynagorny <sergey.s.nagorny@gmail.com>
2021-02-05 14:42:18 +0300

    Подключил редьюсеры в `index.js`

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import thunk from "redux-thunk";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+
+import {createAPI} from "./api.js";
+import {history as BrowserHistory} from "./history";
+
+import reducer from "./reducer/reducer.js";
+import {Operation as DataOperation} from "./reducer/data/data";
+import {ActionCreator as UserActionCreator, AuthorizationStatus} from "./reducer/user/user";
 
 import App from "./components/app/app";
-import {reducer} from "./reducer.js";
+
+
+const onUnauthorized = (error) => {
+  store.dispatch(UserActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+  if (error.config.url !== `/login`) {
+    BrowserHistory.push(`/login`);
+  }
+};
+
+const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
+
 
 const init = () => {
   ReactDOM.render(
@@ -20,4 +41,8 @@ const init = () => {
   );
 };
 
-init();
+Promise.all([
+  store.dispatch(DataOperation.loadPromo()),
+  store.dispatch(DataOperation.loadFilms()),
+]).then(() => init());
+
